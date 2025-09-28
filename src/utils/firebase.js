5b609e4435fefaca27df0a313a33f1241354bfe2
Y.T <tarunyelleti@gmail.com>
2025-09-28 19:12:46 +0530

    validate sign in and sign up and integrated firebase

---
+++ src/utils/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDks73B4V-5t-Xc6jppPiv495gFp8TWVVU",
+  authDomain: "netflixgpt-2f236.firebaseapp.com",
+  projectId: "netflixgpt-2f236",
+  storageBucket: "netflixgpt-2f236.firebasestorage.app",
+  messagingSenderId: "35869436035",
+  appId: "1:35869436035:web:7555ee32c9acb8a63d13b6",
+  measurementId: "G-W2BCTJ24Z2",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
---
+++ src/utils/validate.js
@@ -0,0 +1,20 @@
+export const checkValidSignIn = ({ email, password, name, isSignUp }) => {
+  if (!email || !password) {
+    return isSignUp
+      ? "Name, Email and Password are required"
+      : "Email and Password are required";
+  }
+  const isName = /^[a-zA-Z][a-zA-Z]{2,29}$/.test(name);
+  const isEmailValid = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(
+    email
+  );
+  const isPasswordValid =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,15}$/.test(
+      password
+    );
+  if (isSignUp && !isName) return "Invalid Name";
+  if (!isEmailValid) return "Invalid Email";
+  if (!isPasswordValid)
+    return "Password must be 8–15 characters long and include at least one uppercase letter, one lowercase letter, one digit, and one special character. Spaces are not allowed.";
+  return null;
+};
